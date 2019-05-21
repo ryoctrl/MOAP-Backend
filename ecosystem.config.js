d922fada24d4daf94750e732fcf1b40feb31c811
mosin <ryoctrl@gmail.com>
2019-05-21 12:16:08 +0900

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
             name: 'MOAP-Backend',
             script: 'npm',
             args: 'start',
-            watch: ['controllers/', 'routes/'],
+            watch: ['controllers/', 'routes/', 'models/'],
         }
     ]
 }
