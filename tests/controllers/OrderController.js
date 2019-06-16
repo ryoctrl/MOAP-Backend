834b917ab1a9b67e344b502cd2200dc98b63023f
mosin <ryoctrl@gmail.com>
2019-06-16 16:31:54 +0900

    stash commit

---
+++ tests/controllers/OrderController.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+
+const testCart = [
+    id: 11,
+    name: 'Tapioca Milk-Tea',
+    price: 160,
+    stocks: 50,
+    required_time: 120,
+    image: '7be71691fc36e71759dfdadaf24542cd',
+    createdAt: '2019-04-13T19:09:17.000Z',
+    updatedAt: '2019-04-13T19:09:17.000Z',
+    amount: 2
+];
+
+describe('OrderController', () => {
+    it('_validateCartItems', function() {
+
+    });
+});
